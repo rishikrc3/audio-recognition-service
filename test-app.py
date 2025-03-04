95c200d9f4b64fc16107265aaadecaab85d2fab6
rishik <rishikrc3@gmail.com>
2025-03-04 13:43:29 +0000

    test-added

--- test-app.py
+++ test-app.py
@@ -7,7 +7,6 @@ BASE_URL = "http://localhost:5002/recognize"
 class TestRecognitionService(unittest.TestCase):
 
     def test_recognize_success(self):
-       
         with open("/Users/rishik/Desktop/Catelogue/wavs/~Blinding Lights.wav", "rb") as f:
             files = {"file": ("Blinding_Lights.wav", f, "audio/wav")}
             rsp = requests.post(BASE_URL, files=files)
@@ -44,6 +43,14 @@ class TestRecognitionService(unittest.TestCase):
         
         self.assertEqual(rsp.status_code, 404)
         self.assertIn("Track recognized but not found", rsp.json()["error"])
+
+    def test_recognize_invalid_file_type(self):
+        with open("/Users/rishik/Desktop/Catelogue/wavs/catelog.py", "rb") as f:
+            files = {"file": ("test_image.png", f, "image/png")}  
+            rsp = requests.post(BASE_URL, files=files)
+        
+        self.assertEqual(rsp.status_code, 415) 
+        self.assertIn("Invalid file type", rsp.json()["error"])
 
 
 if __name__ == "__main__":
